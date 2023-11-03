a97f1c39e47802db88624d7142c971d1cecee14f
Sophie <sophie.garreta@gmail.com>
2023-11-03 10:51:02 +0100

    init entity

---
+++ src/Entity/Contribution_aléatoire.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+class Contribution_aléatoire extends Base
+{
+    protected $tableName = APP_TABLE_PREFIX . 'contribution_aléatoire';
+    protected static $instance;
+
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+
+}
