9111150846e3692d5bd4d0fc23fbe76049204b4e
DemoTheMann <joshua.bienvenu18@gmail.com>
2023-11-21 10:43:27 +0100

    updated contrib

--- src/Model/ContributionModel.php
+++ src/Model/ContributionModel.php
@@ -2,14 +2,28 @@
 
 namespace App\Model;
 use App\Entity\Cadavre;
+use App\Entity\Contribution;
+use App\Entity\ContributionAleatoire;
 use DateTime;
+use App\Model\CadavreModel;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class ContributionModel extends Model
 {
 
-
+    public static function getRandom(int $id_joueur)
+    {
+        $activeCadavre = CadavreModel::getInstance()->getActiveCadavre();
+        $contribAleaModel = ContributionAleatoire::getInstance();
+        $contribAleatoire = $contribAleaModel->findBy(
+            [
+                'id_joueur' => $id_joueur,
+                'id_cadavre' => $activeCadavre['id_cadavre'],
+            ])[0];
+        $random = Contribution::getInstance()->findBy(['id_contribution'=>$contribAleatoire['num_contribution']]);
+        return $random[0];
+    }
 
     
     public static function getActiveCadavre(): ?array
@@ -26,5 +40,12 @@ class ContributionModel extends Model
                 return $c;
             }
         }
+    }
+
+    public static function countContrib(int $id_cadavre)
+    {
+        $activeCadavre = Cadavre::getInstance()->findBy(['id_cadavre'=>$id_cadavre])[0];
+        $cadavreCountContrib = count(Contribution::getInstance()->findBy(['id_cadavre' => $activeCadavre['id_cadavre']]));
+        return $cadavreCountContrib;
     }
 }
