cff2e692bd45a3724770e926f1c3b54dfbd9cc47
DemoTheMann <joshua.bienvenu18@gmail.com>
2023-11-29 12:00:53 +0100

    updated api controller

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -11,46 +11,33 @@ class APIController extends Controller
 {
     public function getCadavres()
     {
-        // try {
-            $data = [];
-            
-            $cadavres = Cadavre::getInstance()->findAll();
+        $data = [];    
+        $cadavres = Cadavre::getInstance()->findAll();
 
-            foreach($cadavres as $cadavre)
+        foreach($cadavres as $cadavre)
+        {
+            $cadavre['contributions'] = [];
+            $cadavre['joueurs'] = [];
+            $contributions = ContributionModel::getInstance()->getContribs($cadavre['id_cadavre']);
+
+            foreach($contributions as $contribution)
             {
-                $cadavre['contributions'] = [];
-                $cadavre['joueurs'] = [];
-                $contributions = ContributionModel::getInstance()->getContribs($cadavre['id_cadavre']);
-                // var_dump($contributions);
-
-                foreach($contributions as $contribution)
+                array_push($cadavre['contributions'], $contribution['contributions']);
+                if($contribution['joueurs'])
                 {
-                    // var_dump($contribution);
-                    array_push($cadavre['contributions'], $contribution['contributions']);
-                    if($contribution['joueurs'])
-                    {
-                        array_push($cadavre['joueurs'], $contribution['joueurs']);
-                    }
+                    array_push($cadavre['joueurs'], $contribution['joueurs']);
                 }
-                // var_dump($contribs);
-
-                array_push($data, $cadavre);
-
             }
 
-            $data = json_encode($data);
+            array_push($data, $cadavre);
 
-            header('Content-Type: application/json');
-            echo $data;
-            exit;
+        }
 
-        // } catch (PDOException $e) {
-        //     // Handle database connection errors
-        //     http_response_code(500);
-        //     echo 'Internal Server Error';
-        //     exit;
-        // }
+        $data = json_encode($data);
 
+        header('Content-Type: application/json');
+        echo $data;
+        exit;
     }
 
     public function getCadavreById($id)
