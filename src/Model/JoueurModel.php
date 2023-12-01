c96f21edd03cffbc3d6636e187bdd08491a8af9e
Sophie <sophie.garreta@gmail.com>
2023-12-01 10:34:27 +0100

    contribution for player fixed if cadavre too long

--- src/Model/JoueurModel.php
+++ src/Model/JoueurModel.php
@@ -72,59 +72,6 @@ class JoueurModel
         return $dates;
     }
 
-    public function canPlayId(int $id_joueur)
-    {
-        $ajd = date('Y-m-d');
-        switch ($id_joueur) {
-            case 1:
-            case 2:
-            case 3:
-            //case 4:
-                $dates = [
-                    "date_debut" => "2023-12-07",
-                    "date_fin" => "2023-12-08"
-                ];
-                break;
-            case 5:
-            case 6:
-            case 7:
-            case 8:
-                $dates = [
-                    "date_debut" => "2023-12-09",
-                    "date_fin" => "2023-12-10"
-                ];
-                break;
-            case 9:
-            case 10:
-            case 11:
-            case 12:
-                $dates = [
-                    "date_debut" => "2023-12-11",
-                    "date_fin" => "2023-12-12"
-                ];
-                break;
-            case 13:
-            case 14:
-            case 15:
-            case 16:
-                $dates = [
-                    "date_debut" => "2023-12-13",
-                    "date_fin" => "2023-12-14"
-                ];
-                break;
-            case 4:
-                $dates = [
-                    "date_debut" => "2023-11-30",
-                    "date_fin" => "2023-12-01"
-                ];
-                break;
-            default:
-                break;
-        }
-
-        return $dates;
-    }
-
     public static function getUserName(int $id_joueur)
     {
         $userInfo = Joueur::getInstance()->findBy(['id_joueur' => $id_joueur]);
